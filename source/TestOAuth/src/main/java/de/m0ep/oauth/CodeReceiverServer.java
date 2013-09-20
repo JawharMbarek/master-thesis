9f1d0fb4ba743095a4f0ac24f035ae593eb1154e
Florian Müller <email.mufl@gmail.com>
2013-09-20 18:56:16 +0200

    sync

--- source/TestOAuth/src/main/java/de/m0ep/oauth/CodeReceiverServer.java
+++ source/TestOAuth/src/main/java/de/m0ep/oauth/CodeReceiverServer.java
@@ -17,113 +17,114 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class CodeReceiverServer extends AbstractHandler {
 
-    public static final String CALLBACK_PATH = "/auth/callback";
+	public static final String CALLBACK_PATH = "/auth/callback";
 
-    private String host;
-    private int port;
-    private Server server;
+	private final String host;
+	private int port;
+	private Server server;
 
-    private Lock lock = new ReentrantLock();
-    private Condition codeReceived = lock.newCondition();
+	private final Lock lock = new ReentrantLock();
+	private final Condition codeReceived = lock.newCondition();
 
-    private String error;
-    private String code;
+	private String error;
+	private String code;
 
-    public CodeReceiverServer() {
-	this("localhost", -1);
-    }
-
-    public CodeReceiverServer(final String host, final int port) {
-	this.host = host;
-	this.port = port;
-    }
-
-    private int getFreePort() throws IOException {
-	Socket socket = new Socket();
-	socket.bind(null);
-
-	try {
-	    return socket.getLocalPort();
-	} finally {
-	    socket.close();
-	}
-    }
-
-    public String getRedirectUlAndStartServer() throws Exception {
-	if (-1 == port) {
-	    port = getFreePort();
+	public CodeReceiverServer() {
+		this( "localhost", -1 );
 	}
 
-	server = new Server(port);
-	server.setHandler(this);
-	server.start();
-
-	return "http://" + host + ":" + port + CALLBACK_PATH;
-    }
-
-    public String waitForCode() throws Exception {
-	lock.lock();
-
-	try {
-	    while (null == code && null == error) {
-		codeReceived.awaitUninterruptibly();
-	    }
-
-	    if (null != error) {
-		throw new Exception(error);
-	    }
-
-	    return code;
-	} finally {
-	    lock.unlock();
+	public CodeReceiverServer( final String host, final int port ) {
+		this.host = host;
+		this.port = port;
 	}
-    }
 
-    public void stopServer() throws Exception {
-	if (null != server) {
-	    server.stop();
-	    server = null;
+	private int getFreePort() throws IOException {
+		Socket socket = new Socket();
+		socket.bind( null );
+
+		try {
+			return socket.getLocalPort();
+		} finally {
+			socket.close();
+		}
 	}
-    }
 
-    public void handle(String target, Request baseRequest,
-	    HttpServletRequest request, HttpServletResponse response)
-	    throws IOException, ServletException {
+	public String getRedirectUlAndStartServer() throws Exception {
+		if ( -1 == port ) {
+			port = getFreePort();
+		}
 
-	if (CALLBACK_PATH.equals(target)) {
-	    writeLandingHtml(response);
-	    response.flushBuffer();
+		server = new Server( port );
+		server.setHandler( this );
+		server.start();
 
-	    ((Request) request).setHandled(true);
-
-	    lock.lock();
-	    try {
-		code = request.getParameter("code");
-		error = request.getParameter("error");
-		codeReceived.signal();
-	    } finally {
-		lock.unlock();
-	    }
+		return "http://" + host + ":" + port + CALLBACK_PATH;
 	}
-    }
 
-    private void writeLandingHtml(HttpServletResponse response)
-	    throws IOException {
-	response.setStatus(HttpServletResponse.SC_OK);
-	response.setContentType("text/html");
+	public String waitForCode() throws Exception {
+		lock.lock();
 
-	PrintWriter doc = response.getWriter();
-	doc.println("<html>");
-	doc.println("<head><title>OAuth 2.0 Authentication Token Recieved</title></head>");
-	doc.println("<body>");
-	doc.println("Received verification code. Closing...");
-	doc.println("<script type='text/javascript'>");
-	doc.println("window.setTimeout(function() {");
-	doc.println("    window.open('', '_self', ''); window.close(); }, 1000);");
-	doc.println("if (window.opener) { window.opener.checkToken(); }");
-	doc.println("</script>");
-	doc.println("</body>");
-	doc.println("</HTML>");
-	doc.flush();
-    }
+		try {
+			while ( null == code && null == error ) {
+				codeReceived.awaitUninterruptibly();
+			}
+
+			if ( null != error ) {
+				throw new Exception( error );
+			}
+
+			return code;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void stopServer() throws Exception {
+		if ( null != server ) {
+			server.stop();
+			server = null;
+		}
+	}
+
+	@Override
+	public void handle( String target, Request baseRequest,
+	        HttpServletRequest request, HttpServletResponse response )
+	        throws IOException, ServletException {
+
+		if ( CALLBACK_PATH.equals( target ) ) {
+			writeLandingHtml( response );
+			response.flushBuffer();
+
+			( (Request) request ).setHandled( true );
+
+			lock.lock();
+			try {
+				code = request.getParameter( "code" );
+				error = request.getParameter( "error" );
+				codeReceived.signal();
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+
+	private void writeLandingHtml( HttpServletResponse response )
+	        throws IOException {
+		response.setStatus( HttpServletResponse.SC_OK );
+		response.setContentType( "text/html" );
+
+		PrintWriter doc = response.getWriter();
+		doc.println( "<html>" );
+		doc.println( "<head><title>OAuth 2.0 Authentication Token Recieved</title></head>" );
+		doc.println( "<body>" );
+		doc.println( "Received verification code. Closing..." );
+		doc.println( "<script type='text/javascript'>" );
+		doc.println( "window.setTimeout(function() {" );
+		doc.println( "    window.open('', '_self', ''); window.close(); }, 1000);" );
+		doc.println( "if (window.opener) { window.opener.checkToken(); }" );
+		doc.println( "</script>" );
+		doc.println( "</body>" );
+		doc.println( "</HTML>" );
+		doc.flush();
+	}
 }
